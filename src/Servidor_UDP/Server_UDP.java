bba9cc85048f39d8c73f3687232949a8fd0945a8
rafaelmaderr <33357159+rafaelmaderr@users.noreply.github.com>
2018-05-21 10:55:06 -0300

    SERVIDOR_UDP
    função responsavel pela validação do login,o socket para receber os dados e a
    porta para socket

---
+++ src/Servidor_UDP/Server_UDP.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servidor_UDP;
+
+import java.net.DatagramSocket;
+
+/**
+ *
+ * @author rafael
+ */
+public class Server_UDP {
+      DatagramSocket socket = null;
+    static final int porta = 1233;
+    public Server_UDP() {
+
+    }
+    
+    
+}
