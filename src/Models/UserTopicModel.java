93254b20a185a06119b82c110ad5e45678071f9c
Matheus Bianchi Godinho <matbgodinho@gmail.com>
2018-06-02 13:02:22 -0300

    Models - UserTopicModel
    Criação da classe com o modelo de usuário usado na UDP para realização da validação do login.

---
+++ src/Models/UserTopicModel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author Matheus
+ */
+/**
+ *
+ * Classe com o modelo de usuário e tópico usado na UDP para realizar a
+ * validação do login.
+ */
+public class UserTopicModel {
+
+}
