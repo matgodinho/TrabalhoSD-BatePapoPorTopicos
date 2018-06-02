9ce287827da452a1b210f32f9f9e0a9f197859ae
Matheus Bianchi Godinho <matbgodinho@gmail.com>
2018-06-02 13:42:31 -0300

    Models - UserTopicModel
    Atualizações, variáveis, construtores.

--- src/Models/UserTopicModel.java
+++ src/Models/UserTopicModel.java
@@ -5,6 +5,8 @@
  */
 package Models;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Matheus
@@ -14,6 +16,44 @@ package Models;
  * Classe com o modelo de usuário e tópico usado na UDP para realizar a
  * validação do login.
  */
-public class UserTopicModel {
+public class UserTopicModel implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+    private int codusuario;
+    private String Senha;
+    private int codtopico;
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String Senha) {
+        this.Senha = Senha;
+    }
+
+    public UserTopicModel(int codusuario, String senha, int codtopico) {
+        this.codusuario = codusuario;
+        this.codtopico = codtopico;
+        this.Senha = senha;
+    }
+
+    public int getCodusuario() {
+        return codusuario;
+    }
+
+    public void setCodusuario(int codusuario) {
+        this.codusuario = codusuario;
+    }
+
+    public int getCodtopico() {
+        return codtopico;
+    }
+
+    public void setCodtopico(int codtopico) {
+        this.codtopico = codtopico;
+    }
+
+    public String toString() {
+        return "Id = " + getCodusuario() + " Name = " + getCodtopico();
+    }
 }
